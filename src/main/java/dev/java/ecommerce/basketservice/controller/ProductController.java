d25fc1d19e3e1186fe5250049f5c397216e19010
Matheus <matheusferreirasjc@gmail.com>
2025-11-07 19:39:31 -0300

    Initial commit

---
+++ src/main/java/dev/java/ecommerce/basketservice/controller/ProductController.java
@@ -0,0 +1,30 @@
+package dev.java.ecommerce.basketservice.controller;
+
+import dev.java.ecommerce.basketservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private ProductService productService;
+
+    @GetMapping
+    public ResponseEntity<Void> getAll(){
+
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Void> getById(@PathVariable Long id) {
+
+        return ResponseEntity.ok().build();
+
+    }
+}
---
+++ src/main/java/dev/java/ecommerce/basketservice/service/ProductService.java
@@ -0,0 +1,17 @@
+package dev.java.ecommerce.basketservice.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    public void getAll(){
+
+    }
+
+    public void getById(Long id) {
+
+    }
+}
