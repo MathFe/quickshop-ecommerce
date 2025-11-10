b5626a71ab3521e6cda8a219aab50c81bd2a10e5
Matheus <matheusferreirasjc@gmail.com>
2025-11-10 20:52:41 -0300

    feat: Implementa o Caching de dados externos usando Redis

--- src/main/java/dev/java/ecommerce/basketservice/service/ProductService.java
+++ src/main/java/dev/java/ecommerce/basketservice/service/ProductService.java
@@ -3,24 +3,30 @@ package dev.java.ecommerce.basketservice.service;
 import dev.java.ecommerce.basketservice.client.PlatziStoreClient;
 import dev.java.ecommerce.basketservice.client.response.PlatziProductResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ProductService {
 
     private final PlatziStoreClient platziStoreClient;
 
+    @Cacheable(value = "products")
     public List<PlatziProductResponse> getAll(){
+        log.info("Getting all products");
         return platziStoreClient.getAll();
 
 
     }
 
-    public PlatziProductResponse getById(Long id) {
-        return platziStoreClient.getById(id);
+    @Cacheable(value = "product", key = "#productId")
+    public PlatziProductResponse getById(Long productId) {
+        log.info("Getting product with id: {}", productId);
+        return platziStoreClient.getById(productId);
 
     }
 }
